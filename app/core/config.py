53d6732f31fbbcee2075090742189e47d20255df
alisher193193 <alisher193.193@gmail.com>
2026-09-21 18:40:03 +0500

    fix: load settings from container environment

--- app/core/config.py
+++ app/core/config.py
@@ -45,9 +45,9 @@ class Settings(BaseSettings):
     )
 
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
+        env_file=None,
         extra="ignore",
+        case_sensitive=False,
     )
 
 
